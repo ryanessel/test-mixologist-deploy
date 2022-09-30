344f15fcea58bd79c47f8f2d98dd1aadf886288a
Alek Jonas <alekjonas@hotmail.com>
2022-09-29 20:44:37 -0400

    First round of updates

---
+++ app.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const PORT = 3000;
+const path = require('path');
+const cookieParser = require("cookie-parser");
+var flash = require('connect-flash');
+require('dotenv').config();
+
+// =========== connection to DB =============
+
+mongoose
+  .connect('mongodb://localhost/expressApp')
+  .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch(err => console.error('Error connecting to mongo', err));
+
+// ==========================================
+
+// middleware always comes between declarations and routes
+// ========== MIDDLEWARE =============
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static('public'));
+
+app.set("views", __dirname + "/views");
+
+app.set("view engine", "hbs");
+
+// ===================================
+
+
+//========= APP SETUP ================
+app.use(
+    session({
+      secret: '123secret',
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 600000
+      }, // ADDED code below !!!
+      store: MongoStore.create({
+        mongoUrl: 'mongodb://localhost/expressApp'
+      })
+    })
+  );
+
+app.use(cookieParser());
+app.use(flash());
+
+
+app.use(function (req, res, next) {
+  res.locals.theUser = req.session.currentlyLoggedIn;
+  res.locals.errorMessage = req.flash("error");
+  res.locals.successMessage = req.flash("success");
+  next();
+})
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// ===================================
+
+// ============== ROUTES =====================
+
+// this is what determines the prefix to your routes within the file that you are requiring. If you add "/blah" then all the routes in your index file would have to start with /blah before any route defined. ie: you create a route in index.js that has an endpoint of '/home' but you prefixed '/blah' in the app.js to require index.js, your end result of the route would then be www.domainName.com/blah/home
+//       |
+app.use('/', require('./routes/index'));
+
+// ===========================================
+
+app.use((req, res, next) => {
+    // this middleware runs whenever requested page is not available
+    res.status(404).render("not-found-page");
+  });
+
+app.use((err, req, res, next) => {
+    // whenever you call next(err), this middleware will handle the error
+    // always logs the error
+    console.error("ERROR", req.method, req.path, err);
+
+    // only render if the error ocurred before sending the response
+    if (!res.headersSent) {
+      res.status(500).render("error-page", {theError: err});
+    }
+  });
+
+app.listen(PORT || 3000, () => console.log(`Listening on port ${PORT}`));
+
