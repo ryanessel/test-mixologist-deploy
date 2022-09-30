93b57fb8b8cf308e914706a44a95777fb59d3fd3
Julia Dannerth <jdannerth@gmail.com>
2022-09-29 22:10:47 -0400

    login view added

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 //       |
 app.use('/', require('./routes/index'));
 
-// app.use('/', require('./routes/authroutes.js'));
+app.use('/', require('./routes/authroutes.js'));
 
 // ===========================================
 
--- routes/authroutes.js
+++ routes/authroutes.js
@@ -24,5 +24,76 @@ router.post('/signup', (req, res, next)=>{
 });
 
 
+//=======================LOGIN
+router.get('/login', (req, res, next)=>{
+    res.render('auth/login');
+  })
+  
+  router.post('/login', (req, res, next) => {
+    if (req.body.username === '' || req.body.password === '') {
+      req.flash('error', 'Please make sure to fill in both fields');
+      res.redirect('/login');
+      return;
+    }
+   
+    User.findOne({ username: req.body.username })
+      .then(resultFromDB => {
+        if (!resultFromDB) {
+          req.flash('error', 'could not find that username')
+          res.redirect('/login');
+          return;
+        } else if (bcryptjs.compareSync(req.body.password, resultFromDB.password)) {
+          console.log("found user", resultFromDB);
+          req.session.currentlyLoggedIn = resultFromDB;
+          console.log(req.session);
+          req.flash('success', 'Successfully Logged In as ' + resultFromDB.username);
+          res.redirect('/profile');
+          return;
+        } else {
+          req.flash('error', 'this username/password combination could not be authenticated. please try again');
+          res.redirect('/login');
+        }
+      })
+      .catch(error => console.log(error));
+  });
+
+
+  //================ CHANGE PASSWORD
+router.get('/change-password', (req, res, next)=>{
+    res.render("auth/changepassword", {theUser: req.session.currentlyLoggedIn});
+  })
+  
+  
+  router.post('/new-password', (req, res, next)=>{
+  
+    if(req.body.newpass !== req.body.confirmnewpass){
+      res.redirect("/profile")
+      // need to show an error message here but cant yet
+    }
+  
+    User.findById(req.session.currentlyLoggedIn._id)
+    .then(resultFromDB => {
+       if (bcryptjs.compareSync(req.body.oldpass, resultFromDB.password)) {
+        const saltRounds = 12;
+        bcryptjs
+        .genSalt(saltRounds)
+        .then(salt => bcryptjs.hash(req.body.newpass, salt))
+        .then(hashedPassword => {
+          
+          User.findByIdAndUpdate(req.session.currentlyLoggedIn._id, {
+            password: hashedPassword
+          })
+          .then(()=>{
+            res.redirect('/profile');
+  
+          })
+        })
+          .catch((err)=>{
+            next(err);
+          })
+    }
+  })
+  })
+
 
 module.exports = router;
