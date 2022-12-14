ba2ae8972373724badc14ce419a6a6bd15aa3c36
Alek Jonas <alekjonas@hotmail.com>
2022-12-13 21:19:37 -0500

    deployment params

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ require('dotenv').config();
 // =========== connection to DB =============
 
 mongoose
-  .connect('mongodb://localhost/mixologist')
+  .connect(process.env.mongoURL)
   .then(x => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
   .catch(err => console.error('Error connecting to mongo', err));
 
