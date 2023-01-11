8985df5fc803bcc0a21f844496e670e8f62a54f7
Ry Work <spucti19@gmail.com>
2023-01-11 10:12:40 -0500

    adding require app

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const app = require("./app");
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
